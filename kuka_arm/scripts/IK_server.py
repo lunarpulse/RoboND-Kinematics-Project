613fb0f8660f0686901ddb8ba1121b88ca97310b
JUNG Myoungki <myoungki.jung@gmail.com>
2017-07-25 18:38:54 +1000

    added: git ignore
    modified: handle_calculate_IK
    
    - based on the lectures DH parameters inside for.
    & is that everytime looping starts initiating again and again? time consumption might add, later if it is slow, then change these to outside of for loop

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -31,24 +31,99 @@ def handle_calculate_IK(req):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-            # Define DH param symbols
+            # Define DH param symbols ![]()
 
-
+            ##   alpha i-1 -> twist angle between Z i-1 and Z i along X i-1 
+            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+            ##   a i-1 -> link length between Z i-1 and Z i along X i-1
+            a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+            ##   d i -> Link offset between X i-1 and X i along Z i
+            d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
             
             # Joint angle symbols
+            
+            ## theta i -> angle  between X i-1 and X i along Z i
+            theta1, theta2, theta3, theta4, theta5, theta6, theta7 = symbols('theta1:8')
 
-
-      
             # Modified DH params
-
-
+            s = {   alpha0:     0, a0:      0, d1:  0.75,
+                    alpha1: -pi/2, a1:   0.35, d2:     0, theta2: theta2-pi/2,
+                    alpha2:     0, a2:   1.25, d3:     0, 
+                    alpha3: -pi/2, a3: -0.054, d4:   1.5,
+                    alpha4:  pi/2, a4:      0, d5:     0,
+                    alpha5: -pi/2, a5:      0, d6:     0,
+                    alpha6:     0, a6:      0, d7: 0.303, theta7:       0       }
             
             # Define Modified DH Transformation matrix
+            ##  Correction of URDF vs. DH convention
 
+            ## 90 degree rotation about the y-axis
+            R_yaxis = Matrix([[ cos(-pi/2), 0, sin(-pi/2), 0],
+                        [          0, 1,          0, 0],
+                        [-sin(-pi/2), 0, cos(-pi/2), 0],
+                        [          0, 0,          0, 1]])
+            ## 180 degree rotation about the z-axis
+            R_zaxis = Matrix([[cos(pi), -sin(pi), 0, 0],
+                        [sin(pi),  cos(pi), 0, 0],
+                        [      0,        0, 1, 0],
+                        [      0,        0, 0, 1]])
+            
+            R_correlation = simplify(R_zaxis * R_yaxis)
+            
+            ## Corrected DH convention to URDF frame
+            T_corrected = simplify(T0_G * R_correlation) 
 
 
             # Create individual transformation matrices
 
+            T0_1 = Matrix([[            cos(theta1),            -sin(theta1),            0,              a0],
+                        [sin(theta1)*cos(alpha0), cos(theta1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                        [sin(theta1)*sin(alpha0), cos(theta1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                        [                  0,                   0,            0,               1]])
+            T0_1 = T0_1.subs(s)
+
+            T1_2 = Matrix([[            cos(theta2),            -sin(theta2),            0,              a1],
+                        [sin(theta2)*cos(alpha1), cos(theta2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                        [sin(theta2)*sin(alpha1), cos(theta2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                        [                  0,                   0,            0,               1]])
+            T1_2 = T1_2.subs(s)
+
+            T2_3 = Matrix([[            cos(theta3),            -sin(theta3),            0,              a2],
+                        [sin(theta3)*cos(alpha2), cos(theta3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                        [sin(theta3)*sin(alpha2), cos(theta3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                        [                  0,                   0,            0,               1]])
+            T2_3 = T2_3.subs(s)
+
+            T3_4 = Matrix([[            cos(theta4),            -sin(theta4),            0,              a3],
+                        [sin(theta4)*cos(alpha3), cos(theta4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                        [sin(theta4)*sin(alpha3), cos(theta4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                        [                  0,                   0,            0,               1]])
+            T3_4 = T3_4.subs(s)
+
+            T4_5 = Matrix([[            cos(theta5),            -sin(theta5),            0,              a4],
+                        [sin(theta5)*cos(alpha4), cos(theta5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                        [sin(theta5)*sin(alpha4), cos(theta5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                        [                  0,                   0,            0,               1]])
+            T4_5 = T4_5.subs(s)
+                
+            T5_6 = Matrix([[            cos(theta6),            -sin(theta6),            0,              a5],
+                        [sin(theta6)*cos(alpha5), cos(theta6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                        [sin(theta6)*sin(alpha5), cos(theta6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                        [                  0,                   0,            0,               1]])
+            T5_6 = T5_6.subs(s)
+
+            T6_G = Matrix([[            cos(theta7),            -sin(theta7),            0,              a6],
+                        [sin(theta7)*cos(alpha6), cos(theta7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                        [sin(theta7)*sin(alpha6), cos(theta7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                        [                  0,                   0,            0,               1]])
+            T6_G = T6_G.subs(s)
+
+            T0_2 = simplify(T0_1 * T1_2)
+            T0_3 = simplify(T0_2 * T2_3)
+            T0_4 = simplify(T0_3 * T3_4)
+            T0_5 = simplify(T0_4 * T4_5)
+            T0_6 = simplify(T0_5 * T5_6)
+            T0_G = simplify(T0_6 * T6_G)
 
             
             # Extract end-effector position and orientation from request
